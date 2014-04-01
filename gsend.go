7c51d3ec71069d0274ad41ed66331037403cf63f
He Liu <liulonnie@gmail.com>
2014-03-31 19:22:13 -0700

    start

---
+++ gsend.go
@@ -0,0 +1,35 @@
+package gsend
+
+import (
+	"bytes"
+	"fmt"
+	"net/smtp"
+)
+
+type Gmail struct {
+	From string
+	To string
+	Subject string
+	Message string
+
+	Password string
+}
+
+func (self *Gmail) Send() error {
+	body := new(bytes.Buffer)
+	fmt.Fprintf(body, "To: %s\r\n", self.To)
+	fmt.Fprintf(body, "Subject: %s\r\n", self.Subject)
+	fmt.Fprintf(body, "\r\n")
+	fmt.Fprintf(body, "%s", self.Message)
+
+	const server = "smtp.gmail.com"
+	auth := smtp.PlainAuth("", self.From, self.Password, server)
+	return smtp.SendMail(
+		fmt.Sprintf("%s:587", server),
+		auth, 
+		self.From,
+		[]string{self.To}, 
+		body.Bytes(),
+	)
+}
+	
