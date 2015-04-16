a8cd931f8598b870350b500710739ce5275b52d1
Joe <git@joedeblasio.com>
2015-04-16 11:29:52 -0700

    Adding rudimentary support for differe usernames/from fields

--- gsend.go
+++ gsend.go
@@ -12,22 +12,24 @@ type Gmail struct {
 	Subject string
 	Message string
 
+	Username string
 	Password string
 }
 
 func (self *Gmail) Send() error {
 	body := new(bytes.Buffer)
 	fmt.Fprintf(body, "To: %s\r\n", self.To)
+	fmt.Fprintf(body, "From: %s\r\n", self.From)
 	fmt.Fprintf(body, "Subject: %s\r\n", self.Subject)
 	fmt.Fprintf(body, "\r\n")
 	fmt.Fprintf(body, "%s", self.Message)
 
 	const server = "smtp.gmail.com"
-	auth := smtp.PlainAuth("", self.From, self.Password, server)
+	auth := smtp.PlainAuth("", self.Username, self.Password, server)
 	return smtp.SendMail(
 		fmt.Sprintf("%s:587", server),
 		auth,
-		self.From,
+		self.Username,
 		[]string{self.To},
 		body.Bytes(),
 	)
